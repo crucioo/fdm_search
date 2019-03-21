be9deb748440296a4cd5ea31aaf580b0f4076ab7
cuixinyuan <1225930531@qq.com>
2019-03-21 18:36:34 +0800

    修改首页header组件

--- src/components/utils/Header/index.js
+++ src/components/utils/Header/index.js
@@ -3,9 +3,41 @@ import React,{ Component } from 'react';
 class Header extends Component{
     render(){
         return(
-            <div id="header">
-                用户
-            </div>
+            <header id="header-container">
+                <div id="header-content">
+                    <a className="top-logo">
+                        <img src={require("../../../assets/img/logo.svg")} />    
+                    </a>
+                    <nav className="top-nav">
+                        <ul className="top-nav-content">
+                            <li className="top-menu">
+                                <ul>
+                                    <li>
+                                        <a>首页</a>
+                                    </li>
+                                    <li>
+                                        <a>知识</a>
+                                    </li>
+                                    <li>
+                                        <a>问题</a>
+                                    </li>
+                                    <li>
+                                        <a>我的</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="top-search">
+                                <form>
+                                    <input />
+                                </form>
+                            </li>
+                            <li>
+                                写文章
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
         )
     }
 }
