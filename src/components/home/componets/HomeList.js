0b49fe99d028a5e0f2f34eb0d1524cca5d825824
cuixinyuan <1225930531@qq.com>
2019-05-21 17:15:51 +0800

    封装下拉分页组件

--- src/components/home/componets/HomeList.js
+++ src/components/home/componets/HomeList.js
@@ -1,55 +1,104 @@
-import React from 'react';
-import Good from '../../../assets/img/good.svg';
+import React, { Component } from 'react';
+import Good from '../../../assets/img/good.svg'; 
 import Gooded from '../../../assets/img/gooded.svg';
 import Comment from '../../../assets/img/comment.svg';
 
-const HomeList = ({list,handleGood})=> {
-    return(
-        <ul className="homelist">
-            {list.map(function(listItem){
-                return(
-                    <li key={listItem.id} className="homelist-item">
-                        <a href={`#/article/${listItem.id}`}>
-                            <div className="item-container">
-                                <div className="list-item-left">
-                                    <div className="item-meta">
-                                        <ul>
-                                            {listItem.isHot?<li className="meta-hot">热</li>:''}
-                                            <li className="meta-type">{listItem.type}</li>
-                                            <li>{listItem.time}</li>
-                                            <li>{listItem.label}</li>
-                                        </ul>
+let clientHeight,contentHeight;
+
+function throttleScroll(){
+    var canRun=true;
+    return function(){
+      if(!canRun){
+        return;
+      }
+      canRun=false;
+      setTimeout(function(){
+        contentHeight = document.getElementsByClassName('homelist')[0].lastChild.getBoundingClientRect().bottom;
+        if(contentHeight<=clientHeight){
+            alert(22)
+            /*异步请求 */
+        }
+        canRun=true;
+      },250);
+    }
+  }
+
+  function throttleResize(){
+    var canRun=true;
+    return function(){
+      if(!canRun){
+        return;
+      }
+      canRun=false;
+      setTimeout(function(){
+        clientHeight = document.documentElement.clientHeight;
+        canRun=true;
+      },250);
+    }
+  }
+
+class HomeList extends Component {
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+        clientHeight = document.documentElement.clientHeight;
+        window.onscroll = throttleScroll();
+        window.onresize = throttleResize();
+    }
+    componentDidUpdate(){
+
+    }
+    render(){
+        const{list,handleGood} = this.props;
+
+        return(
+            <ul className="homelist">
+                {list.map(function(listItem){
+                    return(
+                        <li key={listItem.id} className="homelist-item">
+                            <a href={`#/article/${listItem.id}`}>
+                                <div className="item-container">
+                                    <div className="list-item-left">
+                                        <div className="item-meta">
+                                            <ul>
+                                                {listItem.isHot?<li className="meta-hot">热</li>:''}
+                                                <li className="meta-type">{listItem.type}</li>
+                                                <li>{listItem.time}</li>
+                                                <li>{listItem.label}</li>
+                                            </ul>
+                                        </div>
+                                        <div className="item-title">
+                                            <div><strong>{listItem.title}</strong></div>
+                                        </div>
+                                        <div className="item-action">
+                                            <ul>
+                                                <li>
+                                                    <div onClick={handleGood.bind(null,listItem)}>
+                                                        <img src={(listItem.isGood)?Gooded:Good}/>
+                                                        <span className={(listItem.isGood)?"goodSum active":"goodSum"}>{listItem.goodSum}</span>
+                                                    </div>
+                                                </li>
+                                                {/* <li>
+                                                    <div>
+                                                        <img src={Comment}/>
+                                                        <span>{listItem.commentSum}</span>
+                                                    </div>
+                                                </li> */}
+                                            </ul>
+                                        </div>
                                     </div>
-                                    <div className="item-title">
-                                        <div><strong>{listItem.title}</strong></div>
-                                    </div>
-                                    <div className="item-action">
-                                        <ul>
-                                            <li>
-                                                <div onClick={handleGood.bind(null,listItem)}>
-                                                    <img src={(listItem.isGood)?Gooded:Good}/>
-                                                    <span className={(listItem.isGood)?"goodSum active":"goodSum"}>{listItem.goodSum}</span>
-                                                </div>
-                                            </li>
-                                            {/* <li>
-                                                <div>
-                                                    <img src={Comment}/>
-                                                    <span>{listItem.commentSum}</span>
-                                                </div>
-                                            </li> */}
-                                        </ul>
+                                    <div className="list-item-right" style={{backgroundImage:"url("+listItem.pic+")"}}>
                                     </div>
                                 </div>
-                                <div className="list-item-right" style={{backgroundImage:"url("+listItem.pic+")"}}>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                )
-            })}
-        </ul>
-        
-    )
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
+            
+        )
+    }
 }
 
 export default HomeList;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,103 +1,31 @@
 import React,{ Component,PureComponent } from 'react';
 import HomeList from './componets/HomeList';
 import Sider from './componets/Sider';
-
-const list=[{
-    id:1,
-    title:"关于贷款的五级分类",
-    isHot:true,
-    isGood:false,
-    type:"知识",
-    label:"信贷/贷款五级分类",
-    goodSum:69,
-    commentSum:23,
-    time:"3分钟前",
-    pic:"https://user-gold-cdn.xitu.io/2019/3/27/169bb255233ce443?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1"
-},{
-    id:2,
-    title:"质押和抵押的区别",
-    isHot:true,
-    isGood:false,
-    type:"知识",
-    label:"信贷/质押/抵押",
-    goodSum:11,
-    commentSum:23,
-    time:"23分钟前",
-    pic:""
-},{
-    id:3,
-    title:"什么是应降为降？",
-    isHot:false,
-    isGood:false,
-    type:"问题",
-    label:"贷款",
-    goodSum:23,
-    commentSum:23,
-    time:"1小时前",
-    pic:"https://user-gold-cdn.xitu.io/2019/3/27/169bb255233ce443?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1"
-},{
-    id:4,
-    title:"什么是展期？",
-    isHot:false,
-    isGood:false,
-    type:"问题",
-    label:"信贷",
-    goodSum:64,
-    commentSum:23,
-    time:"1天前",
-    pic:""
-},{
-    id:5,
-    title:"什么是阿尔法套利？",
-    isHot:false,
-    isGood:false,
-    type:"问题",
-    label:"证券/股票",
-    goodSum:64,
-    commentSum:23,
-    time:"2天前",
-    pic:""
-},{
-    id:6,
-    title:"什么是金融期货？",
-    isHot:false,
-    isGood:false,
-    type:"问题",
-    label:"证券/期货",
-    goodSum:64,
-    commentSum:23,
-    time:"1天前",
-    pic:"https://user-gold-cdn.xitu.io/2019/3/27/169bb255233ce443?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1"
-},{
-    id:7,
-    title:"金融期货的主要种类有哪些金融期货的主要种类有哪些金融期货的主要种类有哪些金融期货的主要种类有哪些金融期货的主要种类有哪些金融期货的主要种类有哪些金融期货的主要种类有哪些？",
-    isHot:false,
-    isGood:false,
-    type:"问题",
-    label:"证券/期货",
-    goodSum:64,
-    commentSum:23,
-    time:"1小时前",
-    pic:"https://user-gold-cdn.xitu.io/2019/3/27/169bb255233ce443?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1"
-},{
-    id:8,
-    title:"金融期货交易与金融现货交易的区别是什么？",
-    isHot:false,
-    isGood:false,
-    type:"问题",
-    label:"证券/期货",
-    goodSum:64,
-    commentSum:23,
-    time:"1天前",
-    pic:"https://user-gold-cdn.xitu.io/2019/3/27/169bb255233ce443?imageView2/1/w/120/h/120/q/85/format/webp/interlace/1"
-}];
+import Axios from 'axios';
+import Mock from '../../mock/mock';
 
 class HomeBody extends PureComponent{
     constructor(){
         super();
         this.state={
-            list:list
+            list:[]
         }
+    }
+
+    componentDidMount() {
+        var _this = this;
+        Axios.get('/fdm/homelist.json',{
+            params: { 
+                page: 1,
+                size: 10
+             }
+        })
+        .then(function(data){
+            _this.setState({list:data.data});
+        })
+        .catch(function(err){
+            console.log(err);
+        })
     }
     
     handleGood=(listItem,e)=>{
@@ -118,7 +46,8 @@ class HomeBody extends PureComponent{
     render(){
         const homeListProp ={
             list:this.state.list,
-            handleGood:this.handleGood
+            handleGood:this.handleGood,
+
         }
         return(
             <main>
